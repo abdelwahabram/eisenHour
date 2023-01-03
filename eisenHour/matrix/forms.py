ac03c2f6c0d7cf10d201519e4cf66a5ea59687fb
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-03 14:03:36 +0200

    added UserRegistrationForm

---
+++ eisenHour/matrix/forms.py
@@ -0,0 +1,10 @@
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class UserRegistrationForm(UserCreationForm):
+	email = forms.EmailField(required=True)
+	class Meta:
+		model = User
+		fields = ("username", "email", "password1", "password2")
