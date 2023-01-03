43d0ed465f33efba9de99cdba928a95d16a63ed7
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-03 14:03:57 +0200

    added 'register' view

--- eisenHour/matrix/views.py
+++ eisenHour/matrix/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 def first(request):
     return HttpResponse("awesome")
+
+def register(request):
+    if request.method == "POST":
+        newUserForm = UserRegistrationForm(request.POST)
+        if newUserForm.is_valid():
+            newUserForm.save()
+    return render(request,"registration/register.html",{"form":UserRegistrationForm()})
