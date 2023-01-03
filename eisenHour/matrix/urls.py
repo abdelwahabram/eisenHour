a30719370e83be5cb8cc287de4c5df9d9609d96e
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-03 14:01:34 +0200

    added authentication url patterns

--- eisenHour/matrix/urls.py
+++ eisenHour/matrix/urls.py
@@ -1,6 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("",views.first,name="first"),
+    path("", views.first, name="first"),
+    path("register", views.register, name="register"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
