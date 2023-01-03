6dcf87e778de1f427b3bd99c631e33651266b288
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-03 20:49:09 +0200

    created 'Task' model

--- eisenHour/matrix/models.py
+++ eisenHour/matrix/models.py
@@ -1,7 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 # Create your models here.
 
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=900)
+    date = models.DateField(default=timezone.now)
+    importance = models.BooleanField()
+    status = models.BooleanField(default=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="tasks")
+    def save(self, *args, **kwargs):
+        if self.date < timezone.now().date():
+            raise ValidationError("The date cannot be in the past!")
+        super(Task, self).save(*args, **kwargs)
