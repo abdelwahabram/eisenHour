db9e027bd74abd4eb700f138c50b8ce2fd80c130
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-05 16:56:51 +0200

    added task url pattern

--- eisenHour/matrix/urls.py
+++ eisenHour/matrix/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.first, name="first"),
     path("register", views.register, name="register"),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("task/<int:taskId>", views.showTask, name="task"),
 ]
