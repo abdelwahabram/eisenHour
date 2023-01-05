67913ee2ad5ce115bd7c54b37f80008d5c797d3c
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-05 17:32:47 +0200

    added 'addTask' view

--- eisenHour/matrix/views.py
+++ eisenHour/matrix/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, TaskForm
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from django.core.exceptions import ObjectDoesNotExist
@@ -21,7 +21,21 @@ def register(request):
         return render(request,"registration/register.html",{"form":newUserForm})
     return render(request,"registration/register.html",{"form":UserRegistrationForm()})
 
+
 @login_required
 def showTask(request,taskId):
     task = get_object_or_404(Task, id = taskId, user = request.user)
     return render(request,"matrix/task.html",{"task":task})
+
+
+@login_required
+def addTask(request):
+    if request.method == "POST":
+        newTask = Task(user = request.user, status = True)
+        taskForm = TaskForm(request.POST, instance=newTask)
+        if taskForm.is_valid():
+            taskForm.save()
+            return redirect("first")
+            # TODO: update with home screen url
+        return render(request,"matrix/add.html",{"form":taskForm})
+    return render(request,"matrix/add.html",{"form":TaskForm()})
