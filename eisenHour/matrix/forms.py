478af0b9deac39193a946167b8a881e1a0f936b3
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-05 17:32:26 +0200

    added 'TaskForm'

--- eisenHour/matrix/forms.py
+++ eisenHour/matrix/forms.py
@@ -1,6 +1,7 @@
-from .models import User
+from .models import User, Task
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.forms import ModelForm, widgets
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -8,3 +9,12 @@ class UserRegistrationForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ("username", "email", "password1", "password2")
+
+
+class TaskForm(ModelForm):
+	class Meta:
+		model = Task
+		fields = ["title", "description", "date", "importance"]
+		widgets = {
+            'date': widgets.DateInput(attrs={'type': 'date'})
+        }
