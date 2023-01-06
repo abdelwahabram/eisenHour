859ba4ea1fa4013d9bea18dcf3d7bdfdf189fcd0
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-06 11:02:42 +0200

    added 'done' url

--- eisenHour/matrix/urls.py
+++ eisenHour/matrix/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("task/<int:taskId>", views.showTask, name="task"),
     path("task/add", views.addTask, name="add"),
     path("edit/<int:taskId>", views.editTask, name="edit"),
+    path("done/<int:taskId>", views.finishTask, name="done"),    
 ]
