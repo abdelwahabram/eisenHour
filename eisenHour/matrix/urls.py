3970b5ef72e3989fc01340cc3970348220f97b00
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-07 16:08:50 +0200

    added archive url

--- eisenHour/matrix/urls.py
+++ eisenHour/matrix/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", views.first, name="first"),
-    path("matrix", views.showMyMatrix, name="matrix"), 
+    path("matrix", views.showMyMatrix, name="matrix"),
     path("register", views.register, name="register"),
     path('accounts/', include('django.contrib.auth.urls')),
     path("task/<int:taskId>", views.showTask, name="task"),
     path("task/add", views.addTask, name="add"),
     path("edit/<int:taskId>", views.editTask, name="edit"),
     path("done/<int:taskId>", views.finishTask, name="done"),
-    path("change-duration", views.changeUrgencyTimeRange,name="duration")    
+    path("change-duration", views.changeUrgencyTimeRange, name="duration"),
+    path("archive", views.showArchive, name="archive"),
 ]
