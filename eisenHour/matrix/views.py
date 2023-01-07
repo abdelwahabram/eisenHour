278a0d0e735c5d5ebcc9eeca54a4f15c203788dd
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-07 11:10:48 +0200

    format document

--- eisenHour/matrix/views.py
+++ eisenHour/matrix/views.py
@@ -11,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 def first(request):
     return HttpResponse("awesome")
 
+
 def register(request):
     if request.method == "POST":
         newUserForm = UserRegistrationForm(request.POST)
@@ -18,47 +19,47 @@ def register(request):
             newUserForm.save()
             return redirect("first")
             # TODO: update with home screen url
-        return render(request,"registration/register.html",{"form":newUserForm})
-    return render(request,"registration/register.html",{"form":UserRegistrationForm()})
+        return render(request, "registration/register.html", {"form": newUserForm})
+    return render(request, "registration/register.html", {"form": UserRegistrationForm()})
 
 
 @login_required
-def showTask(request,taskId):
-    task = get_object_or_404(Task, id = taskId, user = request.user)
-    return render(request,"matrix/task.html",{"task":task})
+def showTask(request, taskId):
+    task = get_object_or_404(Task, id=taskId, user=request.user)
+    return render(request, "matrix/task.html", {"task": task})
 
 
 @login_required
 def addTask(request):
     if request.method == "POST":
-        newTask = Task(user = request.user, status = True)
+        newTask = Task(user=request.user, status=True)
         taskForm = TaskForm(request.POST, instance=newTask)
         if taskForm.is_valid():
             taskForm.save()
             return redirect("first")
             # TODO: update with home screen url
-        return render(request,"matrix/add.html",{"form":taskForm})
-    return render(request,"matrix/add.html",{"form":TaskForm()})
+        return render(request, "matrix/add.html", {"form": taskForm})
+    return render(request, "matrix/add.html", {"form": TaskForm()})
 
 
 @login_required
 def editTask(request, taskId):
-    task = get_object_or_404(Task, id = taskId, user = request.user)
+    task = get_object_or_404(Task, id=taskId, user=request.user)
     if request.method == "POST":
         taskForm = TaskForm(request.POST, instance=task)
         if taskForm.is_valid():
             taskForm.save()
             return redirect("first")
             # TODO: update with home screen url
-        return render(request,"matrix/edit.html",{"form":taskForm,"taskId":taskId})
-    return render(request,"matrix/edit.html",{"form":TaskForm(instance=task),"taskId":taskId})
+        return render(request, "matrix/edit.html", {"form": taskForm, "taskId": taskId})
+    return render(request, "matrix/edit.html", {"form": TaskForm(instance=task), "taskId": taskId})
 
 
 @login_required
 def finishTask(request, taskId):
-    task = get_object_or_404(Task, id = taskId, user = request.user)
+    task = get_object_or_404(Task, id=taskId, user=request.user)
     if request.method == "POST":
-        task.status = False if task.status == True else  True
+        task.status = False if task.status == True else True
         task.save()
         return redirect("task", taskId)
     return HttpResponseNotAllowed("wrong operation")
