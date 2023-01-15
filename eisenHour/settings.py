77c257ddbe6629cc166ae6236434163870dc6106
abdelwahabram <abdelwahab2000r@gmail.com>
2023-01-15 16:00:16 +0200

    updated settings

--- eisenHour/settings.py
+++ eisenHour/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--cqsinx39mjp6v$4nbi^9j4kg7u44hkai=x#_oj-d)0fb*d*k0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
